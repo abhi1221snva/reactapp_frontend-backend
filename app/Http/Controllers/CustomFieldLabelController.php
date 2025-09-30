dc1a3e3fb8f728971640c076ec36545a441e5cff
Shikha <shikhachhabra12342gmail.com>
2025-09-30 12:51:14 +0530

    https://cafmotel.atlassian.net/browse/DV-111

--- app/Http/Controllers/CustomFieldLabelController.php
+++ app/Http/Controllers/CustomFieldLabelController.php
@@ -73,7 +73,7 @@ public function index(Request $request)
     $custom_field_labels = $query->get()->toArray();
 
     return $this->successResponse("Custom Field Labels List", [
-        'total rows' => $totalRows,
+        'total_rows' => $totalRows,
         'data'  => $custom_field_labels
     ]);
 }
