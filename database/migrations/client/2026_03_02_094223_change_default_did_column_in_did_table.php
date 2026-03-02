1f409211bee0b76b0137b141abb03d7af3d3b44e
Shikha <shikhachhabra12342gmail.com>
2026-03-02 15:19:23 +0530

    migration

---
+++ database/migrations/client/2026_03_02_094223_change_default_did_column_in_did_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         DB::statement("
+            ALTER TABLE did 
+            MODIFY default_did VARCHAR(2) DEFAULT '0'
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+       DB::statement("
+            ALTER TABLE did 
+            MODIFY default_did VARCHAR(2) NULL DEFAULT NULL
+        ");
+    }
+};
